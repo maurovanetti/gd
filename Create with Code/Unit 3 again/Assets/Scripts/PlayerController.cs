b173dc9e956809a008549eb51f0ee18549e70186
Mauro Vanetti <mauro_vanetti@docenti.naba.it>
2023-04-26 12:47:48 +0200

    Unit 3 completed

--- Create with Code/Unit 3 again/Assets/Scripts/PlayerController.cs
+++ Create with Code/Unit 3 again/Assets/Scripts/PlayerController.cs
@@ -5,39 +5,59 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
+    private AudioSource playerAudio;
     public float jumpForce;
     public float gravityModifier;
     public bool isOnGround;
     public bool isGameOver;
+    public ParticleSystem explosionParticles;
+    public ParticleSystem dirtParticles;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
 
     // Start is called before the first frame update
     void Start()
     {
         isOnGround = true;
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
         Physics.gravity = Physics.gravity * gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isOnGround && Input.GetKeyDown(KeyCode.Space))
+        if (isOnGround && !isGameOver && Input.GetKeyDown(KeyCode.Space))
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+            dirtParticles.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
-        {
+        {            
             isOnGround = true;
+            if (!isGameOver)
+            {
+                dirtParticles.Play();
+            }
         } 
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
+            explosionParticles.Play();
+            dirtParticles.Stop();
             isGameOver = true;
             Debug.Log("Game Over!");
+            playerAnim.SetInteger("DeathType_int", 1);
+            playerAnim.SetBool("Death_b", true);
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 }
